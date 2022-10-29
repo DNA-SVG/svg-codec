368d4d7fb351c82153d6f96f0970b696512fad8c
Rzz <99022210+RzZ123@users.noreply.github.com>
2022-10-29 22:26:41 +0800

    Add files via upload

--- encode_svg.py
+++ encode_svg.py
@@ -1,12 +1,8 @@
-from distutils.command.build_scripts import first_line_re
 import encode_tag
 from concurrent.futures.process import BrokenProcessPool
 import xml.etree.ElementTree as ET
 # file = '../sample.xml'
 # file = '../building-construction-education-svgrepo-com.svg'
-file = '../test.svg'
-tree = ET.parse(file)
-root = tree.getroot()
 
 
 def visit(root, bro_counter, my_counter):
@@ -15,26 +11,9 @@ def visit(root, bro_counter, my_counter):
     else:
         first_child = None
     return encode_tag.func(root.tag, root, my_counter, first_child, bro_counter)
-    # if len(root) >= 1:
-    #     if bro == None:
-    #         # print(root.tag, '子节点:'+root[0].tag, None)
-    #         return get_par.func(root.tag, root, first_child, bro)
-
-    #     else:
-    #         # print(root.tag, '子节点:'+root[0].tag, '兄弟节点'+bro.tag)
-    #         return get_par.func(root.tag, root, first_child, bro)
-    # elif len(root) == 0:
-    #     return get_par.func(root.tag, root, None, bro)
-    # if bro == None:
-    #     # print(root.tag, '无子节点', None)
-    #     return get_par.func(root.tag, root, None, bro)
-
-    # else:
-    #     return get_par.func(root.tag, root, None, bro)
-    #     # print(root.tag, '无子节点', '兄弟节点'+bro.tag)
 
 
-counter = 1
+counter = 0
 # 深度遍历
 
 
@@ -61,6 +40,11 @@ def dfs(root, bro_counter=0, my_counter=0):
     return DNA_seq
 
 
-# print(type(dfs(root)))
-# print(dfs(root))
-print("\n".join(dfs(root[0][0], None, 0)))
+if __name__ == '__main__':
+    file = '../test1.svg'
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    a = dfs(root, None, 0)
+    print(a)
+# print("\n".join(a))
