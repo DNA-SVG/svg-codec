1580ba1cac77ba71ad19e723264da3fa752261ec
RzZ123 <1375735389@qq.com>
2022-11-23 17:28:19 +0800

    rebase

--- svg/encode_svg.py
+++ svg/encode_svg.py
@@ -1,50 +1,32 @@
-import encode_tag
 from concurrent.futures.process import BrokenProcessPool
 import xml.etree.ElementTree as ET
 from svg_code import *
-
-# def visit(root, bro_counter, first_child,my_counter):
-#     # if len(root) >= 1:
-#     #     first_child = my_counter + 1
-#     # else:
-#     #     first_child = None
-#     return encode_tag.func(root.tag, root, my_counter, first_child, bro_counter)
-
-
 
 counter = 1
 # 深度遍历
 
 
-def dfs(root, bro_counter=-1,child_counter=-1, my_counter=-1):
+def dfs(root, bro_counter=-1, child_counter=-1, my_counter=-1):
     global counter
     DNA_seq = []
-   
+
     if len(root) == 0:
-        # print('结束')
-        # print(root.tag,my_counter,bro_counter,child_counter)
-        # a = visit(root, bro_counter, None,my_counter)
         a = encode_tag(root, my_counter, -1, bro_counter)
         if a != None:
             DNA_seq += [a]
-            
         return DNA_seq
     else:
         length = len(root)
         counters = range(counter, counter+length)
-        child_counter = counter  
+        child_counter = counter
         counter += length
-        
-       
+
         a = encode_tag(root, my_counter, child_counter, bro_counter)
-        # print(root.tag,my_counter,bro_counter,child_counter)
         if a != None:
             DNA_seq += [a]
-            
         for i in range(length-1):
-            DNA_seq += dfs(root[i], counters[i+1], child_counter,counters[i])
-        DNA_seq += dfs(root[length-1], -1,child_counter, counters[length-1])
-        
+            DNA_seq += dfs(root[i], counters[i+1], child_counter, counters[i])
+        DNA_seq += dfs(root[length-1], -1, child_counter, counters[length-1])
 
     return DNA_seq
 
@@ -54,6 +36,5 @@ if __name__ == '__main__':
     tree = ET.parse(file)
     root = tree.getroot()
 
-    a = dfs(root, -1,1, 0)
+    a = dfs(root, -1, 1, 0)
     print(a)
-# print("\n".join(a))
