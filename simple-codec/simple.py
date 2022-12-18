c8081539df405af8e2ab5bf1784d9f67c25cf9d2
Mingkai Dong <mk@dong.mk>
2022-12-18 18:57:11 +0800

    split to 200-strands in simple-codec and add test-images

--- simple-codec/simple.py
+++ simple-codec/simple.py
@@ -4,6 +4,9 @@ import argparse
 import re
 import sys
 
+CONST_PRIMER_LEN = 6
+CONST_SEQ_MAX_LEN = 200
+
 NT_BITS = {
     "A": "00",
     "T": "11",
@@ -14,6 +17,39 @@ BITS_NT = {}
 for (k, v) in NT_BITS.items():
     BITS_NT[v] = k;
 
+
+def split(long_seq, max_len):
+    """
+    Split a long seq into multiple ones within the max_len
+    """
+    data_len = max_len - CONST_PRIMER_LEN
+    long_seq
+    def num2nt(num):
+        ret = ""
+        while num > 0:
+            ret += 'ATCG'[num % 4]
+            num //= 4
+        return (ret + "A" * CONST_PRIMER_LEN)[:CONST_PRIMER_LEN]
+    return [(num2nt(i//data_len) + long_seq[i:i+data_len])
+            for i in range(0, len(long_seq), data_len) ]
+
+def combine(data):
+    def nt2num(nts):
+        num = 0
+        for nt in reversed(nts):
+            num *= 4
+            num += 'ATCG'.find(nt)
+        return num
+    kv = {}
+    for s in data.split('\n'):
+        num = nt2num(s[:CONST_PRIMER_LEN])
+        kv[num] = s[CONST_PRIMER_LEN:]
+    combined = ""
+    i = 0
+    while i in kv.keys():
+        combined += kv[i]
+        i += 1
+    return combined
 
 def encode(data: bytes):
     """
@@ -47,13 +83,14 @@ def encode_file(infile, outfile):
     with open(infile, "rb") as f:
         data = f.read()
     seq = encode(data)
+    seq = "\n".join(split(seq, CONST_SEQ_MAX_LEN))
     with open(outfile, "w") as f:
         f.write(seq)
-    
 
 def decode_file(infile, outfile):
     with open(infile, "r") as f:
         seq = f.read()
+    seq = combine(seq)
     data = decode(seq)
     with open(outfile, "wb") as f:
         f.write(data)
