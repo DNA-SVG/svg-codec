e4a13042f57336867ebadec1b7f41e3713d6fb8b
restid <864485349.qq.com>
2023-12-25 15:26:07 +0800

    更新collect工具

--- codec/collect.py
+++ codec/collect.py
@@ -1,34 +1,39 @@
 class CollectMethod:
-    collect_dict = {}
-    collect_number = 0
+    __collect_dict = {}
+    __collect_number = 0
 
     @classmethod
     def dict_collect(cls, str):
-        if str in cls.collect_dict:
-            cls.collect_dict[str] += 1
+        if str in cls.__collect_dict:
+            cls.__collect_dict[str] += 1
         else:
-            cls.collect_dict[str] = 1
-    
+            cls.__collect_dict[str] = 1
+        
     @classmethod
     def double_dict_collect(cls, k1, k2):
-        if (k1, k2) in cls.collect_dict:
-            cls.collect_dict[k1, k2] += 1
+        if (k1, k2) in cls.__collect_dict:
+            cls.__collect_dict[k1, k2] += 1
         else:
-            cls.collect_dict[k1, k2] = 1
+            cls.__collect_dict[k1, k2] = 1
     
     @classmethod
     def number_collect(cls, number):
-        cls.collect_number += number
+        cls.__collect_number += number
 
     @classmethod
     def return_collect(self):
-        if len(self.collect_dict) > 0:
-            sorted_dict = sorted(self.collect_dict.items(), key=lambda x: x[1], reverse=True)
+        if len(self.__collect_dict) > 0:
+            sorted_dict = sorted(self.__collect_dict.items(), key=lambda x: x[1], reverse=True)
             for key, value in sorted_dict:
                 print(key, value)
-        print(self.collect_number)
+        print(self.__collect_number)
     
     @classmethod
     def return_double_collect(self):
-        for k1, k2 in self.collect_dict:
-            print(k1, k2, self.collect_dict[k1, k2])
+        for k1, k2 in self.__collect_dict:
+            print(k1, k2, self.__collect_dict[k1, k2])
+    
+    @classmethod
+    def clear_collect(cls):
+        cls.__collect_dict.clear()
+        cls.__collect_number = 0
